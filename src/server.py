1fa656e4fa9180476909183cc3e44f00053ce2ed
Jon Dickson <JonDickson20@gmail.com>
2022-03-11 19:19:36 -0500

    testing detector

--- src/server.py
+++ src/server.py
@@ -4,6 +4,7 @@ import struct
 import cv2
 import time
 import os
+import numpy as np
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,9 +14,96 @@ frame_count = 0
 total_frames = 0
 fps = -1
 
+
+INPUT_WIDTH = 640
+INPUT_HEIGHT = 640
+SCORE_THRESHOLD = 0.2
+NMS_THRESHOLD = 0.05
+CONFIDENCE_THRESHOLD = 0.05
+
+def build_model():
+    net = cv2.dnn.readNet("../config_files/bestRac0305.onnx")
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+    return net
+
+def load_classes():
+    class_list = []
+    with open("../config_files/RacClasses.txt", "r") as f:
+        class_list = [cname.strip() for cname in f.readlines()]
+    print(class_list)
+    return class_list
+
+def detect(image, net):
+    blob = cv2.dnn.blobFromImage(image, 1/255.0, (INPUT_WIDTH, INPUT_HEIGHT), swapRB=True, crop=False)
+    net.setInput(blob)
+    preds = net.forward()
+    return preds
+
+class_list = load_classes()
+
+def wrap_detection(input_image, output_data):
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    rows = output_data.shape[0]
+
+    image_width, image_height, _ = input_image.shape
+
+    x_factor = image_width / INPUT_WIDTH
+    y_factor =  image_height / INPUT_HEIGHT
+
+    for r in range(rows):
+        row = output_data[r]
+        confidence = row[4]
+        if confidence >= 0.4:
+
+            classes_scores = row[5:]
+            _, _, _, max_indx = cv2.minMaxLoc(classes_scores)
+            class_id = max_indx[1]
+            if (classes_scores[class_id] > .25):
+
+                confidences.append(confidence)
+
+                class_ids.append(class_id)
+
+                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item()
+                left = int((x - 0.5 * w) * x_factor)
+                top = int((y - 0.5 * h) * y_factor)
+                width = int(w * x_factor)
+                height = int(h * y_factor)
+                box = np.array([left, top, width, height])
+                boxes.append(box)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45)
+
+    result_class_ids = []
+    result_confidences = []
+    result_boxes = []
+
+    for i in indexes:
+        result_confidences.append(confidences[i])
+        result_class_ids.append(class_ids[i])
+        result_boxes.append(boxes[i])
+
+    return result_class_ids, result_confidences, result_boxes
+
+def format_yolov5(frame):
+    row, col, _ = frame.shape
+    _max = max(col, row)
+    result = np.zeros((_max, _max, 3), np.uint8)
+    result[0:row, 0:col] = frame
+    return result
+
+
+colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
+net = build_model()
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
-s.bind((os.environ.get("HOST_ADDRESS"), os.environ.get("HOST_PORT")))
+s.bind((os.environ.get("HOST_ADDRESS"), int(os.environ.get("HOST_PORT"))))
 print('Socket bind complete')
 s.listen(10)
 print('Socket now listening')
@@ -24,6 +112,8 @@ conn, addr = s.accept()
 
 data = b'' ### CHANGED
 payload_size = struct.calcsize("L") ### CHANGED
+
+
 
 while True:
     frame_count += 1
@@ -63,6 +153,19 @@ while True:
         fps_label = "FPS: %.2f" % fps
         cv2.putText(frame, fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
+    inputImage = format_yolov5(frame)
+    outs = detect(inputImage, net)
+
+    class_ids, confidences, boxes = wrap_detection(inputImage, outs[0])
+
+    frame_count += 1
+    total_frames += 1
+
+    for (classid, confidence, box) in zip(class_ids, confidences, boxes):
+        color = colors[int(classid) % len(colors)]
+        cv2.rectangle(frame, box, color, 2)
+        cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
+        cv2.putText(frame, class_list[classid], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
 
     cv2.imshow('frame', frame)
     cv2.waitKey(1)
