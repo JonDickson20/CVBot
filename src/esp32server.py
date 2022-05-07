29f29ec595676358bba027267b1d06dd9f1c1540
Jon Dickson <JonDickson20@gmail.com>
2022-05-07 09:22:44 -0400

    refactoring

--- src/esp32server.py
+++ src/esp32server.py
@@ -7,9 +7,7 @@ import datetime
 import os
 import sys
 import numpy as np
-from PIL import Image
-from io import BytesIO
-import base64
+
 from time import sleep
 from dotenv import load_dotenv
 
@@ -21,7 +19,8 @@ start = time.time_ns()
 frame_count = 0
 total_frames = 0
 fps = -1
-
+last_packet = b''
+connected = False
 
 INPUT_WIDTH = 640
 INPUT_HEIGHT = 640
@@ -44,7 +43,6 @@ AIM_MAX_DEGREES = 40 #this is most degrees aim will turn in 1 command
 DELAY_BETWEEN_COMMANDS = 2000
 
 colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
-connected = False
 aim = ["STOP", "STOP", 0, 0, 0]
 target_left_frame = 100 #target has been missing for this many frames
 last_command = datetime.datetime.now() 
@@ -62,13 +60,12 @@ def load_classes():
 	print(class_list)
 	return class_list
 
+
 def detect(image, net):
 	blob = cv2.dnn.blobFromImage(image, 1/255.0, (INPUT_WIDTH, INPUT_HEIGHT), swapRB=True, crop=False)
 	net.setInput(blob)
 	preds = net.forward()
 	return preds
-
-class_list = load_classes()
 
 def wrap_detection(input_image, output_data):
 	class_ids = []
@@ -120,6 +117,7 @@ def wrap_detection(input_image, output_data):
 
 	return result_class_ids, result_confidences, result_boxes
 
+
 def format_yolov5(frame):
 	row, col, _ = frame.shape
 	_max = max(col, row)
@@ -127,36 +125,31 @@ def format_yolov5(frame):
 	result[0:row, 0:col] = frame
 	return result
 
+def connect():
+	global connected
 
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(5)
+	s.bind((os.environ.get("HOST_ADDRESS"), int(os.environ.get("HOST_PORT"))))
+	s.listen(10)
+	print('Socket now listening')
 
-net = build_model()
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
-print('Socket created')
-s.bind((os.environ.get("HOST_ADDRESS"), int(os.environ.get("HOST_PORT"))))
-print('Socket bind complete')
-s.listen(10)
-print('Socket now listening')
-
-while True:
-	target_in_frame = 0
-	while not connected:  
-		# attempt to reconnect, otherwise sleep for 2 seconds  
+	while not connected:
 		try:
 			print('waiting for connection')
 			conn, addr = s.accept()
 			print(addr)
-			last_packet = b''
-			payload_size = struct.calcsize("P")
-			connected = True  
+			connected = True
 			print('client connected')
-		except socket.error:  
-			sleep( 2 )  
-#	try:
+		except socket.error:
+			sleep( 1 )
+
+	return conn
+
+
+def get_frame():
+	global last_packet
 	buffer_size = 10000
-	output = False
 	data = last_packet
 	while True:
 		packet = conn.recv(buffer_size)
@@ -164,14 +157,16 @@ while True:
 		if packet[0] == 255 and data != b'':
 			last_packet = packet
 			break
-
-	imgnp=np.array(bytearray(data),dtype=np.uint8)
-	frame=cv2.imdecode(imgnp,-1)
+	image_array=np.array(bytearray(data),dtype=np.uint8)
+	frame=cv2.imdecode(image_array,-1)
+	# todo some frames are bad. found here. fix?
 	if isinstance(frame,type(None)):
-		print(len(data))
-		continue
+		frame = get_frame()
 
-	#FPS
+	return frame
+
+def show_fps():
+	global frame_count, total_frames, fps, start
 	frame_count += 1
 	total_frames += 1
 	if frame_count >= 30:
@@ -183,83 +178,91 @@ while True:
 	if fps > 0:
 		fps_label = "FPS: %.2f" % fps
 		cv2.putText(frame, fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+	return frame
 
 
-	cv2.imshow('frame', frame)
-	cv2.waitKey(1)
-	continue
+net = build_model()
 
-	inputImage = format_yolov5(frame)
-	outs = detect(inputImage, net)
+conn = connect()
 
-	class_ids, confidences, boxes = wrap_detection(inputImage, outs[0])
+while True:
+	try:
+		frame = get_frame()
+		frame = show_fps()
 
-	frame_count += 1
-	total_frames += 1
-	
-	for (classid, confidence, box) in zip(class_ids, confidences, boxes):
-		color = colors[int(classid) % len(colors)]
-		cv2.rectangle(frame, box, color, 2)
-		cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
-		cv2.putText(frame, class_list[classid], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
-		
-		if class_list[classid]=='person' or class_list[classid] =='dog':
-			target_left_frame = 0
-			target_in_frame = 1
-			frame_height = frame.shape[0] + Y_OFFSET
-			frame_width = frame.shape[1] + X_OFFSET
-			target_center = [int(box[0]+box[2]/2),int(box[1]+box[3]/2)]
-			
-			aim_x_degrees = abs(target_center[0] - frame_width/2)/(frame_width)*AIM_MAX_DEGREES
-			if target_center[0] < frame_width/2 - AIM_DEADZONE_SIZE:
-				aim_x = "LEFT"
-			elif target_center[0] > frame_width/2 + AIM_DEADZONE_SIZE:
-				aim_x = "RIGHT"
-			else:
-				aim_x = "STOP"
-			
-			aim_y_degrees = abs(target_center[1] - frame_height/2)/(frame_height)*AIM_MAX_DEGREES
-			if target_center[1] < frame_height/2 - AIM_DEADZONE_SIZE:
-				aim_y = "UP"
-			elif target_center[1] > frame_height/2 + AIM_DEADZONE_SIZE:
-				aim_y = "DOWN"
-			else:
-				aim_y = "STOP"
-			
-			cv2.circle(frame,target_center,3,(0,0,255),2) #center of target
-			cv2.circle(frame,(int(frame_width/2), int(frame_height/2)),3,(0,255,0),2) #center of frame
-			cv2.rectangle(frame, (target_center[0]-AIM_DEADZONE_SIZE, target_center[1]-AIM_DEADZONE_SIZE), (target_center[0]+AIM_DEADZONE_SIZE, target_center[1]+AIM_DEADZONE_SIZE), (255,0,0), 2)
+		cv2.imshow('frame', frame)
+		cv2.waitKey(1)
+		continue
 
-			x_percent = aim_x_degrees/(AIM_MAX_DEGREES/2)
-			y_percent = aim_y_degrees/(AIM_MAX_DEGREES/2)
-			larger = max(x_percent,y_percent)
-			print(larger)
+		inputImage = format_yolov5(frame)
+		outs = detect(inputImage, net)
 
-			time_diff = datetime.datetime.now() - last_command
-			if int(time_diff.total_seconds() * 1000) > DELAY_BETWEEN_COMMANDS*larger:			
-				if aim_x != "STOP" or aim_y != "STOP" or aim[2] == 0:	
-					aim = [aim_x, aim_y, 1, aim_x_degrees, aim_y_degrees]
-					response = pickle.dumps(aim)
-					conn.sendall(response)
-					last_command = datetime.datetime.now()
-					
-			continue #this handles multiple targets by just dealing with the first one
+		class_ids, confidences, boxes = wrap_detection(inputImage, outs[0])
+		target_in_frame = 0
+		class_list = load_classes()
+		for (classid, confidence, box) in zip(class_ids, confidences, boxes):
+			color = colors[int(classid) % len(colors)]
+			cv2.rectangle(frame, box, color, 2)
+			cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
+			cv2.putText(frame, class_list[classid], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
 
-			
-	#no target, turn off laser
-	if target_in_frame == 0:
-		target_left_frame = target_left_frame + 1
-		if target_left_frame > DISABLE_LASER_AFTER_FRAMES_MISSING and aim[2] == 1:
-			aim[2] = 0
-			aim[0] = 'STOP'
-			aim[1] = 'STOP'
-			aim[3] = 0
-			aim[4] = 0
-			conn.sendall(pickle.dumps(aim))
-	
+			if class_list[classid]=='person' or class_list[classid] =='dog':
+				target_left_frame = 0
+				target_in_frame = 1
+				frame_height = frame.shape[0] + Y_OFFSET
+				frame_width = frame.shape[1] + X_OFFSET
+				target_center = [int(box[0]+box[2]/2),int(box[1]+box[3]/2)]
 
-	cv2.imshow('frame', frame)
-	cv2.waitKey(1)
-#	except Exception as e:
-#		connected = False
-#		print(e)
+				aim_x_degrees = abs(target_center[0] - frame_width/2)/(frame_width)*AIM_MAX_DEGREES
+				if target_center[0] < frame_width/2 - AIM_DEADZONE_SIZE:
+					aim_x = "LEFT"
+				elif target_center[0] > frame_width/2 + AIM_DEADZONE_SIZE:
+					aim_x = "RIGHT"
+				else:
+					aim_x = "STOP"
+
+				aim_y_degrees = abs(target_center[1] - frame_height/2)/(frame_height)*AIM_MAX_DEGREES
+				if target_center[1] < frame_height/2 - AIM_DEADZONE_SIZE:
+					aim_y = "UP"
+				elif target_center[1] > frame_height/2 + AIM_DEADZONE_SIZE:
+					aim_y = "DOWN"
+				else:
+					aim_y = "STOP"
+
+				cv2.circle(frame,target_center,3,(0,0,255),2) #center of target
+				cv2.circle(frame,(int(frame_width/2), int(frame_height/2)),3,(0,255,0),2) #center of frame
+				cv2.rectangle(frame, (target_center[0]-AIM_DEADZONE_SIZE, target_center[1]-AIM_DEADZONE_SIZE), (target_center[0]+AIM_DEADZONE_SIZE, target_center[1]+AIM_DEADZONE_SIZE), (255,0,0), 2)
+
+				x_percent = aim_x_degrees/(AIM_MAX_DEGREES/2)
+				y_percent = aim_y_degrees/(AIM_MAX_DEGREES/2)
+				larger = max(x_percent,y_percent)
+				print(larger)
+
+				time_diff = datetime.datetime.now() - last_command
+				if int(time_diff.total_seconds() * 1000) > DELAY_BETWEEN_COMMANDS*larger:
+					if aim_x != "STOP" or aim_y != "STOP" or aim[2] == 0:
+						aim = [aim_x, aim_y, 1, aim_x_degrees, aim_y_degrees]
+						response = pickle.dumps(aim)
+						conn.sendall(response)
+						last_command = datetime.datetime.now()
+
+				continue #this handles multiple targets by just dealing with the first one
+
+
+		#no target, turn off laser
+		if target_in_frame == 0:
+			target_left_frame = target_left_frame + 1
+			if target_left_frame > DISABLE_LASER_AFTER_FRAMES_MISSING and aim[2] == 1:
+				aim[2] = 0
+				aim[0] = 'STOP'
+				aim[1] = 'STOP'
+				aim[3] = 0
+				aim[4] = 0
+				conn.sendall(pickle.dumps(aim))
+
+
+		cv2.imshow('frame', frame)
+		cv2.waitKey(1)
+	except Exception as e:
+		connected = False
+		print(e)
